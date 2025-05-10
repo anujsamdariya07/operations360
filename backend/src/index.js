c025ddfc6b257fbc4a581a6a11cfaecd6f2336a9
anujsamdariya07 <anujsamdariya07@gmail.com>
2025-05-10 20:36:47 +0530

    add cookie-parser and cors middleware for enhanced request handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+
 const dotenv = require('dotenv').config();
+
 const dbConnect = require('./config/dbConnect');
+
 const authRoutes = require('./routes/authRoutes');
 const organizationRoutes = require('./routes/organizationRoutes');
 const itemRoutes = require('./routes/itemRoutes');
@@ -9,10 +12,22 @@ const messageRoutes = require('./routes/messageRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const customerRoutes = require('./routes/customerRoutes');
 
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+
+
 dbConnect();
 
 const app = express();
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
